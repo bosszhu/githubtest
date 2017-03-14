33fd98bcef0357505a0d419b082b49d61dd105b1
bosszhu <m18270822163@163.com>
2017-03-14 14:29:01 +0800

    1. 创建Xcode项目

---
+++ xinxinLove/xinxinLove/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xinxinLove
+//
+//  Created by yuntong on 2017/3/14.
+//  Copyright © 2017年 yuntong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ xinxinLove/xinxinLove/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  xinxinLove
+//
+//  Created by yuntong on 2017/3/14.
+//  Copyright © 2017年 yuntong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
